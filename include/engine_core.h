d77f2a613ada29f4f5ed07f30dd21a9686069b10
Chris Viqueira <chrisviqueira@gmail.com>
2014-09-21 21:58:10 -0400

    I really really hate command line

--- include/engine_core.h
+++ include/engine_core.h
@@ -1,3 +1,15 @@
+/*************************************************************
+ * engine_core.h
+ * -------------
+ * Header file for the physics engine core. Defines
+ * vectors and their functions.
+ *
+ * Last Revision: Sept. 19, 2014
+ *
+ * TO DO: - Continue following tutorial to fill this out.
+ *		  - This bullet included for to do list formatting.
+ *************************************************************/
+
 /********************************************************
  * #include guards to protect against double inclusions
  * when using header files
@@ -10,66 +22,42 @@
 
 namespace marballs
 {
-    /**
-     * Holds the values for a 3D vector
-    **/
+    /* CLASS vector3 - Holds the values for a 3D vector */
     class vector3
     {
         /*********************************
          *     Variable Declarations
         *********************************/
         public:
-        
-            /**
-             * Holds the x-axis value
-            **/
-            marb x;
-            
-             /**
-             * Holds the y-axis value
-            **/
-            marb y;
-            
-             /**
-             * Holds the x value
-            **/
-            marb z;
-        
+
+            marb x; // Holds the x-axis value
+            marb y; // Holds the y-axis value
+            marb z; // Holds the z-access value
+
         private:
-        
-            /**
-             * From book:
-             *  For performance purposes, some machines run faster
-             *  with 4 floating point numbers than three. Padding variable.
-            **/
-            marb pad;
-        
+
+            marb pad; // From book: Padding variable for memory performance, optional.
+
         /*********************************
          *     Function Declarations
         *********************************/
         public:
-        
-            /**
-             * Default constructor
-            **/
+
+            // vector3 - Default Constructor
             vector3() : x(0), y(0), z(0);
-            
-            /**
-             * Constructor which enters values that the users sends
-            **/
+
+            // vector3 - Constructor that initializes x, y, and z.
             vector3(marb x, marb y, marb z) : x(x), y(y), z(z);
-            
-            /**
-             * Inverts all the values of the vector
-            **/
+
+            // invert - Inverts a vector3's x, y, and z values.
             vector3 invert()
             {
                 x = -x;
                 y = -y;
                 z = -z;
             }
-            
+
     }
 }
 
-#endif
+#endif  //ENGINE_CORE_INCLUDED
