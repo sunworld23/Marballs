a96fedfa1360775db85b7e8e5b5f307b08a109b3
Brenton Brown <brentonbrown@Brenton-Browns-MacBook-Pro.local>
2014-09-24 17:10:33 -0400

    Implemented the integrate function.

--- include/particle.cpp
+++ include/particle.cpp
@@ -6,5 +6,28 @@
  *
  * Last Revision: Sept. 24, 2014
  *
- * TO DO: - Continue following tutorial to fill this out?
+ * TO DO: - Follow rest of tutorial. (Left off on page 54).
  *************************************************************/
+
+#include <assert.h>
+#include "marballs.h"
+
+using namespace marballs;
+
+void Particle::integrate(marb duration)
+{
+    assert(duration > 0.0);
+    
+    //Updates linear position.
+    position.addScaledVector(velocity, duration);
+    
+    //Work out the acceleration from the force.
+    Vector3 resultingAcc = acceleration;
+    resultingAcc.addScaledVector(forceAccum, inverseMass);
+    
+    //Update linear velocity from the acceleration.
+    velocity.addScaledVector(resultingAcc, duration);
+    
+    //Inflicts drag on the object
+    velocity *= real_pow(damping, duration);
+}
