cc1d1cf86def2e9f5fb5c0a13f8c29406e78fc2a
TheSquishyDitto <squishyditto@yahoo.com>
2014-09-28 22:57:19 -0400

    Fireworks demo now works. See Version History for more details.

---
+++ src/engine_core.cpp
@@ -0,0 +1,15 @@
+/*************************************************************
+ * engine_core.cpp
+ * ---------------
+ * Source code for physics engine core.
+ *
+ * Last Revision: Sept. 28, 2014
+ *
+ * TO DO: - Continue following tutorial to fill this out.
+ *************************************************************/
+
+ #include "engine_core.h"
+
+ using namespace marballs;
+
+ const Vector3 Vector3::GRAVITY = Vector3(0, -9.81, 0);
