5d7f1f69bb9a342ad6d15ce6128d9b9543fc8097
sunworld23 <sunworld23@gmail.com>
2014-10-27 21:50:22 -0400

    engine_core fix

--- src/engine_core.cpp
+++ src/engine_core.cpp
@@ -15,6 +15,18 @@
  const Vector3 Vector3::GRAVITY = Vector3(0, -9.81, 0);
  const Vector3 Vector3::UP = Vector3(0, 1, 0);
 
+ marb marballs::sleepEpsilon = ((marb)0.3);
+
+ void marballs::SetSleepEpsilon(marb value)
+{
+    marballs::sleepEpsilon = value;
+}
+
+marb marballs::GetSleepEpsilon()
+{
+    return marballs::sleepEpsilon;
+}
+
 marb Matrix4::GetDeterminant() const
 {
     return -data[8]*data[5]*data[2]+
@@ -28,22 +40,22 @@ marb Matrix4::GetDeterminant() const
 void Matrix4::SetInverse(const Matrix4 &m)
 {
     // Make sure the determinant is non-zero.
-    real det = getDeterminant();
+    marb det = GetDeterminant();
     if (det == 0) return;
-    det = ((real)1.0)/det;
-    
+    det = ((marb)1.0)/det;
+
     data[0] = (-m.data[9]*m.data[6]+m.data[5]*m.data[10])*det;
     data[4] = (m.data[8]*m.data[6]-m.data[4]*m.data[10])*det;
     data[8] = (-m.data[8]*m.data[5]+m.data[4]*m.data[9])*det;
-    
+
     data[1] = (m.data[9]*m.data[2]-m.data[1]*m.data[10])*det;
     data[5] = (-m.data[8]*m.data[2]+m.data[0]*m.data[10])*det;
     data[9] = (m.data[8]*m.data[1]-m.data[0]*m.data[9])*det;
-    
+
     data[2] = (-m.data[5]*m.data[2]+m.data[1]*m.data[6])*det;
     data[6] = (+m.data[4]*m.data[2]-m.data[0]*m.data[6])*det;
     data[10] = (-m.data[4]*m.data[1]+m.data[0]*m.data[5])*det;
-    
+
     data[3] = (m.data[9]*m.data[6]*m.data[3]
                -m.data[5]*m.data[10]*m.data[3]
                -m.data[9]*m.data[2]*m.data[7]
@@ -62,4 +74,13 @@ void Matrix4::SetInverse(const Matrix4 &m)
                +m.data[0]*m.data[9]*m.data[7]
                +m.data[4]*m.data[1]*m.data[11]
                -m.data[0]*m.data[5]*m.data[11])*det;
+}
+
+Matrix3 Matrix3::LinearInterpolate(const Matrix3& a, const Matrix3& b, marb prop)
+{
+    Matrix3 result;
+    for (unsigned i = 0; i < 9; i++) {
+        result.data[i] = a.data[i] * (1-prop) + b.data[i] * prop;
+    }
+    return result;
 }
